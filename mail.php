5c5562175af33603daeb90590cdfc9e37c1b484b
LewKM <lewismwendwa321@gmail.com>
2023-10-07 18:38:57 +0300

    Adds address information on the website

--- mail.php
+++ mail.php
@@ -12,7 +12,7 @@ $email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 
-$email_body = "$message. Below are the details for plan package, I am $name, this is my email $email. I am interested in a $days day safari to $destination in $month $year. I will be traveling with $adults adults and $children children. I would like $rooms rooms. My budget is $budget. $message";
+$email_body = "Hello, I am $name, this is my email $email. I am interested in a $days day safari to $destination in $month $year. I will be traveling with $adults adults and $children children. I would like $rooms rooms. My budget is $budget. $message";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -23,7 +23,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/mail/SMTP.php';
 
 $mail = new PHPMailer;
 $mail->isSMTP(); 
-$mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+// $mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
 $mail->Host = "smtp.gmail.com"; //  SMTP over IPv6
 $mail->Port = 587; // TLS only
 $mail->SMTPSecure = 'tls'; // ssl is deprecated
@@ -44,7 +44,12 @@ $mail->SMTPOptions = array(
                     )
                 );
 if(!$mail->send()){
-    echo "Mailer Error: " . $mail->ErrorInfo;
+    echo 'alert("Mailer Error: " . $mail->ErrorInfo)';
 }else{
-    echo "Message sent!";
+    echo  '<script type ="text/JavaScript">';  
+    echo 'alert("Message Sent Successfully!")';  
+    echo '</script>';
 }?>
+
+
+
